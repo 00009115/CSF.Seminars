8c9ee07d981981eae2ec6369d1e2e594ce64dc26
00009115 <4BIS1.00009115@gmail.com>
2020-11-11 14:33:21 +0500

    Week 7 seminar tasks

---
+++ Week 7/main.py
@@ -0,0 +1,15 @@
+my_expected_marks = {
+    "CSF": 68,
+    "FunPro": 65,
+    "WebTech": 72
+}
+
+marks = list(my_expected_marks.values())
+
+def calculator():
+    average_mark = 0
+    for mark in marks:
+        average_mark += mark
+    print("Average mark is:", average_mark / len(my_expected_marks))
+
+calculator()
