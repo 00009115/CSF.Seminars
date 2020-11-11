4ec9fe15cd5ca567aa89ce1112044f8b8f80b448
00009115 <4BIS1.00009115@gmail.com>
2020-11-11 14:35:09 +0500

    Update Week 7

--- Week 7/main.py
+++ Week 7/main.py
@@ -1,15 +1,15 @@
-my_expected_marks = {
+my_marks = {
     "CSF": 68,
     "FunPro": 65,
     "WebTech": 72
 }
 
-marks = list(my_expected_marks.values())
+marks = list(my_marks.values())
 
 def calculator():
     average_mark = 0
     for mark in marks:
         average_mark += mark
-    print("Average mark is:", average_mark / len(my_expected_marks))
+    print("Average mark is:", average_mark / len(my_marks))
 
 calculator()
