48a4ff4760f295219735344f93f0122f848b66a2
00009115 <4BIS1.00009115@gmail.com>
2020-11-18 14:26:11 +0500

    Update Week 8

--- Week 8/Task 1/main.py
+++ Week 8/Task 1/main.py
@@ -1,7 +1,9 @@
-s1 = ("18", "00009898", "Samarkand")
-s2 = ("18", "00009597", "Tashkent")
+s1 = (18, 00009898, "Samarkand")
+s2 = (19, 00009597, "Tashkent")
 
 if s1 > s2:
     print("s1 is older than s2")
+elif s1 == s2:
+    print("s1 is in the saem age with s2")
 else:
     print("s1 is younger than s2")
